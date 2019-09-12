8e392053ea9f0f4cc96869d335869fa98f7220f4
jennawilkinson <43350731+jennawilkinson@users.noreply.github.com>
2019-09-12 13:06:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let sessions
+let cartCount = 0
+let inCart = []
+
+function loadPage () {
+    fetch("sessions.json")
+        .then(response => response.json())
+        .then(json => {console.log(json)
+                        sessions = json
+                        createCards()})
+        .catch(error => console.error(error))
+}
+
+function createCards() {
+    let container = document.createElement('div')
+    container.innerHTML = sessions.map(session => 
+    `<div ${session.Spots > 0 ? '' : 'class="empty"'}>
+    <h3>${session.Title}</h3>
+    <h4>${session['Presenter(s)']}</h4>
+    <h5>${session['Area of Focus']}</h5>
+    <h5>${session['Grade']}</h5>
+    <h5>${session['Building']}</h5>
+    <p>${session['Description']}</p>
+    <button ${session.Spots <= 0 ? 'disabled' : ''}
+    onclick="addToCart('${session['Title']}')">Sign Up</button>
+    </div>`
+                                      ).join('')
+    document.querySelector('article')
+        .append(container)
+}
+
+function addToCart(sessionName) {
+    if (!inCart.includes(sessionName)) {
+        inCart.push(SessionName)
+        let item= document.createElem('li')
+        item.innerHTMl = sessionName
+        
+        document.querySelector('ul#cart')
+            .append(item)
+
+        cartCount++
+        let cartCountElem = document.querySelector('span#cartCount')
+        cartCountElem.textContent = cartCount
+        if (cartCount >= 3) {
+            cartCountElem.style.backgroundColor = "green"
+        }
+        else if (cartCount > 0) {
+            cartCountElem.style.backgroundColor = "aqua"
+        }
+    }
+    else {
+        alert("You have already added this session to your cart.")
+    }
+}
