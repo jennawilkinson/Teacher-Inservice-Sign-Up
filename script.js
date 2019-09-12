45582f7f3185b28a938bd9e76f0997458e29a344
jennawilkinson <43350731+jennawilkinson@users.noreply.github.com>
2019-09-12 14:13:27 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
+//create global variable
 let sessions
 let cartCount = 0
 let inCart = []
 
-function loadPage () {
-    fetch("sessions.json")
-        .then(response => response.json())
-        .then(json => {console.log(json)
-                        sessions = json
-                        createCards()})
-        .catch(error => console.error(error))
+function loadPage() {
+fetch("sessions.json")
+    .then(response => response.json())
+    .then(json => {console.log(json)
+                  sessions = json
+                  createCards()})
+    .catch(err => console.error(err))
+//createCards()
 }
-
 function createCards() {
-    let container = document.createElement('div')
-    container.innerHTML = sessions.map(session => 
-    `<div ${session.Spots > 0 ? '' : 'class="empty"'}>
-    <h3>${session.Title}</h3>
-    <h4>${session['Presenter(s)']}</h4>
-    <h5>${session['Area of Focus']}</h5>
+let container = document.createElement('div')
+container.innerHTML = sessions.map(session =>
+`<div ${session.Spots > 0 ? '' : 'class="empty"'}>
+<h3>${session.Title}</h3>
+<h4>Presenter${session['Presenter(s)'].split(' and ').length > 1 ? 's' : ''}: ${session['Presenter(s)']}</h4>
+<h5>${session['Area of Focus']}</h5>
     <h5>${session['Grade']}</h5>
     <h5>${session['Building']}</h5>
-    <p>${session['Description']}</p>
-    <button ${session.Spots <= 0 ? 'disabled' : ''}
+<p>${session['Description']}</p>
+<button 
+    ${session.Spots <= 0 ? 'disabled' : ''}
     onclick="addToCart('${session['Title']}')">Sign Up</button>
-    </div>`
-                                      ).join('')
-    document.querySelector('article')
-        .append(container)
+</div>`
+                                  ).join('')
+document.querySelector('article')
+    .append(container)
 }
 
 function addToCart(sessionName) {
     if (!inCart.includes(sessionName)) {
-        inCart.push(SessionName)
-        let item= document.createElem('li')
-        item.innerHTMl = sessionName
-        
+        inCart.push(sessionName)
+        let item = document.createElement('li')
+        item.innerHTML = sessionName
+
         document.querySelector('ul#cart')
             .append(item)
 
